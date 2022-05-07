b139d8a833d3112fefa9de9ea3b0f02d331eb6a5
Yuuineko <littlenyann@gmail.com>
2022-05-06 21:20:45 -0300

    update-components-with-cypress-script
    renderização do banner, renderização do showcase.
    
    Co-Authored-By: Yuuineko <42789715+yuuineko@users.noreply.github.com>

--- cypress/e2e/home.ts
+++ cypress/e2e/home.ts
@@ -1,10 +1,15 @@
 /// <reference path = "../support/index.d.ts"/>  
 
-describe ('Home Page', () =>{
-    it('should render home sections', () =>{
+describe('Home Page', () => {
+    it('should render home sections', () => {
 
-        //visitar a pagina
-        cy.visit('http://localhost:3000')
+        cy.visit('/')
 
+        cy.shouldRenderBanner()
+        cy.shouldRenderShowcase({name:"New Games"})
+        cy.shouldRenderShowcase({name:"Most Popular Games"})
+        cy.shouldRenderShowcase({name:"Upcoming Games"})
+        cy.shouldRenderShowcase({name:"Free Games"})
+        
     });
-}) ;
+});
