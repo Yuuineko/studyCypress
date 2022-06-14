6c9635103aa4529b3c25c0bae33f670fff42bd2d
Yuuineko <littlenyann@gmail.com>
2022-06-13 21:06:44 -0300

    game-info-cy
    automation game info with cypress

--- cypress/e2e/game.ts
+++ cypress/e2e/game.ts
@@ -1,8 +1,20 @@
 /// <reference path = "../support/index.d.ts"/>  
 
+import { findAllByRole } from "@testing-library/dom";
 
 describe('game page', () => {
     it('should render game page sections', () => {
         cy.visit('/game/cyberpunk-2077')
+
+
+        cy.getByDataCy('game-info').within(() => {
+            cy.findByRole('heading', { name: /cyberpunk 2077/i }).should('exist')
+            cy.findByText(/^Cyberpunk 2077 is an open-world/i).should('exist')   //^  < comeca com 
+            cy.findByText('$59.99').should('exist')
+            cy.findByRole('button', { name: /add to cart/i }).should('exist')
+        })
+        //gallery 
+        cy.findAllByRole('button', { name: /thumb \-/i }).should('have.length.gt', 0)
     });
+
 });
